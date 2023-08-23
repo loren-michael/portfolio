4f7a40f5e4f91e39a6124bf0640d4f1bce49216b
loren <lorenmichaelh@gmail.com>
2023-08-23 11:46:05 -0500

    base application functional

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Home from './components/Home';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Home />
     </div>
   );
 }
