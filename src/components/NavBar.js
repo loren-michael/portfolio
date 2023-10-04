486e00296749b4dcb758e4338708ac75ab064902
loren <lorenmichaelh@gmail.com>
2023-10-04 13:34:27 -0500

    change navbar to test repo change in do

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,7 +6,7 @@ const NavBar = () => {
   return (
     <div class="h-20 bg-hartmanblue-400">
       <div class="w-full">
-        <span class="float-left">Loren Hartman</span>
+        <span class="float-left">This is the portfolio repo</span>
         <span class="float-right">
           <NavLink to="/about" href="/about" class="font-sans text-lg text-white"></NavLink>
           <NavLink to="/resume" href="/resume" class="font-sans text-lg text-white"></NavLink>
